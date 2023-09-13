79a906c980a8724380d42623864984b98f301b88
Agustin Petrillo <agustinpetrillo1@gmail.com>
2023-09-13 19:12:52 -0300

    created new account functional

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,19 +1,25 @@
-import { CreateUser, UserLogin } from "@/types";
-
-export const createUser = async (dataUser: CreateUser) => {
+export const createUser = async (
+  email: string,
+  password: string | number,
+  username: string
+) => {
   await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL_API}/auth/signup`, {
     method: "POST",
-    body: JSON.stringify(dataUser),
+    body: JSON.stringify({
+      email: email,
+      password: password,
+      username: username,
+    }),
     headers: {
       "Content-Type": "application/json",
     },
   });
 };
 
-export const loginUser = async (dataUser: UserLogin) => {
+export const loginUser = async (email: string, username: string) => {
   await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL_API}/auth/login`, {
     method: "POST",
-    body: JSON.stringify(dataUser),
+    body: JSON.stringify({ email: email, username: username }),
     headers: {
       "Content-Type": "application/json",
     },
--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,32 +1,55 @@
 "use client";
 
-import { useState } from "react";
-import { CreateUser } from "@/types";
+import { FormEvent, useRef } from "react";
 
 const SignUp = () => {
-  const [userDataSignUp, setUserDataSignUp] = useState<CreateUser>({
-    username: "",
-    email: "",
-    password: "",
-  });
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+
+    try {
+      await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL_API}/auth/signup`, {
+        method: "POST",
+        body: JSON.stringify({
+          email: formData.get("email"),
+          name: formData.get("name"),
+          password: formData.get("password"),
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (formRef.current) {
+        setTimeout(() => {
+          formRef.current!.reset();
+        }, 1500);
+      }
+    } catch (error) {
+      console.log(`the error was: ${error}`);
+    }
+  };
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-l from-red-950 to-teal-950">
       <div className="w-full max-w-md p-6 rounded-md shadow-md bg-slate-300">
         <h2 className="mb-4 text-2xl font-semibold text-center">Sign Up</h2>
-        <form>
+        <form ref={formRef} onSubmit={(e) => handleSubmit(e)}>
           <div className="mb-4">
             <label
               className="block mb-2 text-sm font-bold text-gray-700"
-              htmlFor="email"
+              htmlFor="username"
             >
               Username
             </label>
             <input
               className="w-full px-3 py-2 leading-tight text-gray-700 border rounded-md focus:outline-none focus:shadow-outline"
-              id="email"
+              id="username"
               type="text"
               placeholder="Enter your username"
+              name="name"
             />
           </div>
           <div className="mb-4">
@@ -41,6 +64,7 @@ const SignUp = () => {
               id="email"
               type="text"
               placeholder="Enter your email"
+              name="email"
             />
           </div>
           <div className="mb-4">
@@ -55,13 +79,11 @@ const SignUp = () => {
               id="password"
               type="password"
               placeholder="Enter your password"
+              name="password"
             />
           </div>
           <div className="mb-4">
-            <button
-              className="px-4 py-2 font-bold text-white bg-blue-500 rounded-md hover:bg-blue-700 focus:outline-none focus:shadow-outline"
-              type="submit"
-            >
+            <button className="px-4 py-2 font-bold text-white bg-blue-500 rounded-md hover:bg-blue-700 focus:outline-none focus:shadow-outline">
               Create Account
             </button>
           </div>
