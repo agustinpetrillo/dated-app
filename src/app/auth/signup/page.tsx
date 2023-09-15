a57ec12f12169289d84856ee1bfaeda2d666fa47
Agustin Petrillo <agustinpetrillo1@gmail.com>
2023-09-15 00:58:19 -0300

    new login axios

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { FormEvent, useRef } from "react";
+import { FormEvent, useState } from "react";
 import { useRouter } from "next/navigation";
+import axios, { AxiosError } from "axios";
 
 const SignUp = () => {
-  const formRef = useRef<HTMLFormElement>(null);
   const router = useRouter();
+  const [error, setError] = useState<string>("");
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -13,36 +14,27 @@ const SignUp = () => {
     const formData = new FormData(e.currentTarget);
 
     try {
-      const res = await fetch(
+      const res = await axios.post(
         `${process.env.NEXT_PUBLIC_BACKEND_URL_API}/auth/signup`,
         {
-          method: "POST",
-          body: JSON.stringify({
-            email: formData.get("email"),
-            name: formData.get("name"),
-            last_name: formData.get("last_name"),
-            password: formData.get("password"),
-          }),
-          headers: {
-            "Content-Type": "application/json",
-          },
+          email: formData.get("email"),
+          name: formData.get("name"),
+          last_name: formData.get("last_name"),
+          password: formData.get("password"),
         }
       );
-      if (res?.ok) return router.push("/home");
-
-      if (formRef.current) {
-        setTimeout(() => {
-          formRef.current!.reset();
-        }, 1500);
-      }
-    } catch (error) {}
+      if (res?.status === 201) return router.push("/home");
+    } catch (error) {
+      if (error instanceof AxiosError) setError(error.response?.data.message);
+    }
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gradient-to-l from-red-950 to-teal-950">
+    <div className="flex items-center justify-center min-h-screen dark:bg-primary">
+      <p className="text-red-500">{error}</p>
       <div className="w-full max-w-md p-6 rounded-md shadow-md bg-slate-300">
         <h2 className="mb-4 text-2xl font-semibold text-center">Sign Up</h2>
-        <form ref={formRef} onSubmit={(e) => handleSubmit(e)}>
+        <form onSubmit={(e) => handleSubmit(e)}>
           <div className="mb-4">
             <label
               className="block mb-2 text-sm font-bold text-gray-700"
